9c4a9dfd035e347bab0a97c03091f173749b3fb0
BransonGitomeh <sirbranson@gmail.com>
2019-08-03 11:46:08 +0300

    fix: use templates

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
-var express = require('express');
-var exphbs = require('express-handlebars');
-var compression = require('compression');
-var minifyHTML = require('express-minify-html');
-var morgan = require("morgan")
-var path = require("path")
+var express = require("express");
+var exphbs = require("express-handlebars");
+var compression = require("compression");
+var minifyHTML = require("express-minify-html");
+var morgan = require("morgan");
 
 var app = express();
 
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
-}));
+app.engine(".hbs", exphbs({ extname: ".hbs", defaultLayout: "main" }));
 
-app.use(express.static('public'))
+app.set("view engine", ".hbs");
 
-app.set('view engine', 'handlebars');
-
-app.use(minifyHTML({
-    override:      true,
+app.use(
+  minifyHTML({
+    override: true,
     htmlMinifier: {
-        removeComments:            true,
-        collapseWhitespace:        true,
-        collapseBooleanAttributes: true,
-        removeAttributeQuotes:     true,
-        removeEmptyAttributes:     true,
-        minifyJS:                  true
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeEmptyAttributes: true,
+      minifyJS: true
     }
-}));
+  })
+);
 
+app.use(morgan("tiny"));
 
-
-app.use(morgan('tiny'))
-
-app.get('/index.html', function(req, res) {
-	res.render('home');
+app.get("/index.html", function(req, res) {
+  res.render("home");
 });
 
-app.get('/foo.html', function(req, res) {
-	res.render('foo');
+app.get("/foo.html", function(req, res) {
+  res.render("foo");
 });
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.listen(3000, function(error) {
-	console.log("server started at 3000")
-	require("./site_generator")(app)
+  console.log("server started at 3000");
+  require("./site_generator")(app);
 });
